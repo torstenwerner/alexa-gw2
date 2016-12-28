4a790bf71c7300651a5945cf8d7ef81b3ed4c281
Torsten Werner <torsten.werner@westernacher.com>
2016-12-28 12:52:40 +0100

    use chai for assertions

--- src/test/gw2.spec.js
+++ src/test/gw2.spec.js
@@ -1,11 +1,11 @@
 'use strict'
 
-const assert = require('assert');
+const should = require('chai').should();
 const gw2 = require('../gw2');
 
 describe('GW2TOKEN', function () {
     it('must be configured', function () {
-        assert(gw2.isTokenConfigured());
+        gw2.isTokenConfigured().should.be.true;
     });
 });
 
@@ -13,17 +13,17 @@ describe('my wallet', function () {
     it('should have a reasonable amount of gold', function () {
         return gw2.fetchCoins()
             .then(item => item.gold)
-            .then(gold => assert(gold > 100 && gold < 1000));
+            .then(gold => gold.should.be.above(100).and.below(1000));
     });
 });
 
 describe('my account', function () {
     it('should return some information about me', function () {
         function check(account) {
-            assert.equal(account.shortName, 'Bollin');
-            assert.equal(account.world, "2201");
-            assert.equal(account.worldName, "Kodasch");
-            assert(account.wvw_rank > 740);
+            account.shortName.should.be.equal('Bollin');
+            account.world.should.be.equal(2201);
+            account.worldName.should.be.equal("Kodasch");
+            account.wvw_rank.should.be.above(740);
             return account;
         }
 
